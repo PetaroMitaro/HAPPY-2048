f194a1e2f510cdf665d81d9fad497c989b7aec87
Peter Mitrano <mitranopeter@gmail.com>
2014-04-20 11:47:00 -0400

    stupid comment error

--- js/game_manager.js
+++ js/game_manager.js
@@ -68,8 +68,8 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    //var value = (int)(Math.pow(2,Math.randNorm()));
-    var value 
+    var value = (int)(Math.pow(2,Math.randNorm()));
+    //var value 
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
